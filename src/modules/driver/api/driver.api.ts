e366835d8336ac22dfa3f95f6850eace8cd5da73
Edison Avilés <eaviles@mentaseg.com>
2023-04-25 13:09:03 -0500

    Improve Crud and manage requests not found's

--- src/modules/driver/api/driver.api.ts
+++ src/modules/driver/api/driver.api.ts
@@ -5,30 +5,28 @@ import { mongo } from "mongoose"
 import DriverAppService from "../application/driver.application"
 import DriverInsert from "../shared/domain/driver.insert"
 import DriverUpdate from "../shared/domain/driver.update"
-import statusCodes from "../../../core/api/statusCodes"
 
 const driverApi = express.Router()
 const driverAppService = new DriverAppService()
-const { OK } = statusCodes
 
 driverApi.get("/:id", async (req: Request, res: Response) => {
     const result = await driverAppService.getDriver(new mongo.ObjectId(req.params.id))
-    res.status(OK).send(result)
+    res.send(result)
 })
 
 driverApi.post("/", async (req: Request, res: Response) => {
     const result = await driverAppService.insertDriver(req.body as DriverInsert)
-    res.status(OK).send(result)
+    res.send(result)
 })
 
 driverApi.put("/", async (req: Request, res: Response) => {
     const result = await driverAppService.updateDriver(req.body as DriverUpdate)
-    res.status(OK).send(result)
+    res.send(result)
 })
 
 driverApi.delete("/:id", async (req: Request, res: Response) => {
     const result = await driverAppService.deleteDriver(new mongo.ObjectId(req.params.id))
-    res.status(OK).send(result)
+    res.send(result)
 })
 
 export default driverApi
---
+++ src/modules/driver/shared/shared.consts.ts
@@ -0,0 +1,5 @@
+const sharedConsts = {
+    id: "id"
+}
+
+export default sharedConsts
